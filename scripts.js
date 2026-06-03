b640f107f74b038058bcc16e0286b4d6a798c04e
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-06-03 17:32:40 +0100

    Refactor win function
    win function now more coherent and checks all win potentials, including ties.
    
    game board now constructs the play area with nested loop.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 const gameBoard = (() => {
-    let board = [[Cell(), Cell(), Cell()],
-                 [Cell(), Cell(), Cell()],
-                 [Cell(), Cell(), Cell()]];
+    const boardSize = 3
+
+    const board = [];
+
+    for (let i = 0; i < boardSize; i++) {
+        board[i] = [];
+        for (let j = 0; j < boardSize; j++) {
+            board[i].push(Cell());
+        }
+    }
 
     
     
@@ -49,6 +56,7 @@ const gameController = (() => {
     let activePlayer = players[0];
     const board = gameBoard;
     const playArea = gameBoard.getBoard();
+    let roundsPlayed = 0;
 
     const addPlayerOne = (name) => {
         players.push(new Player(name, 1))
@@ -76,27 +84,69 @@ const gameController = (() => {
     const playRound = (x, y) => {
  
         if (playArea[x][y].getValue() === 0) {
+            roundsPlayed++
             console.log(
             `Placing ${getActivePlayer().name}'s marker`);
             board.placeMarker(x,y, getActivePlayer().marker);
+            if (checkWin(x, y, getActivePlayer().marker)) {
+                console.log("WIN");
+                gameBoard.printBoard();
+            } else if (roundsPlayed >= 9){
+                console.log("Tie");
+            } else
+            switchPlayer();
+            printNewRound();
+            console.log("rounds played:", roundsPlayed);
+            }
 
+
+        }
+
+    const checkWin = (x,y, player) => {
+        let rowCheck = 0
+        let columnCheck = 0
+        for (let i = 0; i < playArea.length; i++) {
+           
+            if (playArea[x][i].getValue() === player) {
+                rowCheck++;
+            }
+            if (playArea[i][y].getValue() === player) {
+                columnCheck++;
+            }
+        }
+        if ((rowCheck === playArea.length) || (columnCheck === playArea.length)) {
+            console.log("Row or column win");
+            return true;
             
-            if ((playArea[0][0].getValue() && playArea[0][1].getValue() && playArea[0][2].getValue() === getActivePlayer().marker) || 
-                (playArea[1][0].getValue() && playArea[1][1].getValue() && playArea[1][2].getValue() === getActivePlayer().marker) ||
-                (playArea[2][0].getValue() && playArea[2][1].getValue() && playArea[2][2].getValue() === getActivePlayer().marker) ||
-                (playArea[0][0].getValue() && playArea[1][1].getValue() && playArea[2][2].getValue() === getActivePlayer().marker) ||
-                (playArea[2][0].getValue() && playArea[1][1].getValue() && playArea[0][2].getValue() === getActivePlayer().marker)) {
-                console.log("WIN")
-                printNewRound();
-            } else {
-
-            /*switchPlayer();*/
-            printNewRound()};
-        } else {} {console.log("space not free");};
-
-    };
+        }
+        if ((playArea[0][0].getValue() === player && playArea[1][1].getValue() === player 
+        && playArea[2][2].getValue() === player) ||
+        (playArea[0][2].getValue() === player && playArea[1][1].getValue() === player
+        && playArea[2][0].getValue() === player)) {
+            console.log("Diag win");
+            return true;
+        }
+    }
 
 
 
     return {addPlayerOne, addPlayerTwo, getActivePlayer, playRound,}
 })();
+
+
+
+const form = document.getElementById("move");
+
+
+form.addEventListener("submit", tempPlayMove);
+
+form.addEventListener("p1", gameController.addPlayerOne("Player1"));
+form.addEventListener("p2", gameController.addPlayerTwo("Player2"));
+
+function tempPlayMove (e) {
+    e.preventDefault();
+
+    const x = document.getElementById("move").elements['x'].value;
+    const y = document.getElementById("move").elements['y'].value;
+    gameController.playRound(x,y);
+}
